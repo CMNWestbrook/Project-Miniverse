eac3abc1201b218a9533a779f9887cfc17cf63c3
cwestbrooksf <christina@cwestbrook.com>
2017-10-11 08:30:51 +0000

    updated port

--- application.py
+++ application.py
@@ -299,4 +299,6 @@ if __name__ == "__main__":
     # application.debug = True
 
     # application.run(port=5000, host='0.0.0.0')
-    application.run(host='0.0.0.0')
+    PORT = int(os.environ.get("PORT", 5000))
+
+    application.run(host="0.0.0.0", port=PORT)
